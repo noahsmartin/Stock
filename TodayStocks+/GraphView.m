283b41d384839a2f7515c7558b0b5bccf9cf9af3
Noah Martin <Noah@Noahs-MacBook-Pro.local>
2014-08-19 01:07:33 -0700

    Added shadow fill under graph

--- TodayStocks+/GraphView.m
+++ TodayStocks+/GraphView.m
@@ -26,27 +26,52 @@
         [path lineToPoint:NSMakePoint(i*lineSpacing, height)];
         [path stroke];
     }
-    double minPrice = [self.data getMinValue];
-    double maxPrice = [self.data getMaxValue];
-    double span = maxPrice - minPrice;
-    double factor = height/span;
     for(int i = 0; i < count-1; i++) {
-        DataPoint* point = [self.data.points objectAtIndex:i];
-        DataPoint* point2 = [self.data.points objectAtIndex:i+1];
-        double y = point.price - minPrice;
-        double yPixel = y * factor;
-        double y2 = point2.price - minPrice;
-        double yPixel2 = y2 * factor;
+        NSPoint point = [self cordsForDataPoint:[self.data.points objectAtIndex:i]];
+        point.x = i*pointSpacing;
+        NSPoint point2 = [self cordsForDataPoint:[self.data.points objectAtIndex:i+1]];
+        point2.x = (i+1)*pointSpacing;
+        
         path = [[NSBezierPath alloc] init];
-        [path moveToPoint:NSMakePoint(i*pointSpacing, yPixel)];
-        [path lineToPoint:NSMakePoint((i+1)*pointSpacing, yPixel2)];
         [path setLineWidth:0];
+        [path moveToPoint:point];
+        [path lineToPoint:point2];
         [[NSColor whiteColor] setStroke];
         [path stroke];
+    }
+    if(count > 0) {
+        NSPoint point = [self cordsForDataPoint:[self.data.points objectAtIndex:0]];
+        NSBezierPath* path = [[NSBezierPath alloc] init];
+        [path moveToPoint:NSMakePoint(0, 0)];
+        [path lineToPoint:point];
+        for(int i = 1; i < count; i++) {
+            NSPoint point = [self cordsForDataPoint:[self.data.points objectAtIndex:i]];
+            point.x = i*pointSpacing;
+            [path lineToPoint:point];
+        }
+        point = [self cordsForDataPoint:[self.data.points objectAtIndex:count-1]];
+        point.x = (count-1)*pointSpacing;
+        [path lineToPoint:NSMakePoint(point.x, 0)];
+        [path closePath];
+        
+         NSGradient* fillGradient = [[NSGradient alloc] initWithStartingColor:[NSColor colorWithCalibratedRed:0.35 green:0.35 blue:0.35 alpha:0.5] endingColor:[NSColor colorWithCalibratedRed:0.15 green:0.15 blue:0.15 alpha:0.5]];
+        [fillGradient drawInBezierPath:path angle:90];
     }
     
     
     // Drawing code here.
+}
+
+-(NSPoint)cordsForDataPoint:(DataPoint*)point {
+    int height = self.bounds.size.height;
+    double minPrice = [self.data getMinValue];
+    double maxPrice = [self.data getMaxValue];
+    double span = maxPrice - minPrice;
+    double factor = height/span;
+    double y = point.price - minPrice;
+    double yPixel = y * factor;
+    // TODO: actually get the real x value from the timestamp of this point instead of guessing that every point is spread equally across the x axis
+    return NSMakePoint(0, yPixel);
 }
 
 @end
